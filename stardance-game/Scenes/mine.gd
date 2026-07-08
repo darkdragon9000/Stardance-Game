a807aba66fdb5e34ec13a7fbf4a5f14839acafac
Xblader1359 <Xblader1359.Minecraft@gmail.com>
2026-07-08 17:54:54 -0500

    mine slight bugfix

--- stardance-game/Scenes/mine.gd
+++ stardance-game/Scenes/mine.gd
@@ -4,6 +4,8 @@ extends RigidBody2D
 var impulse_applied := false
 var explosion = preload("res://Scenes/explosion.tscn")
 @onready var explode_timer: Timer = $ExplodeTimer
+@onready var area_2d: Area2D = $Area2D
+var hit_by_rocket := false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	explode_timer.start()
@@ -18,14 +20,6 @@ func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
 		impulse_applied = true
 
 
-
-func _on_explode_timer_timeout() -> void:
-	var explosion_instance = explosion.instantiate()
-	explosion_instance.global_position = global_position
-	get_parent().add_child(explosion_instance)
-	queue_free()
-
-
 func _on_body_entered(body: Node) -> void:
 	if body.is_in_group("pistol_bullets"):
 		explode_timer.stop()
@@ -33,11 +27,33 @@ func _on_body_entered(body: Node) -> void:
 		explosion_instance.global_position = global_position
 		get_parent().add_child(explosion_instance)
 		queue_free()
-	if body.is_in_group("rockets") or body.is_in_group("explosions"):
+	if body.is_in_group("rockets"):
+		explode_timer.stop()
+		hit_by_rocket = true
+		var explosion_instance = explosion.instantiate()
+		explosion_instance.global_position = global_position
+		explosion_instance.explosion_strength *= 1.2
+		print(explosion_instance.explosion_strength)
+		explosion_instance.scale *= 1.2
+		get_parent().add_child(explosion_instance)
+		queue_free()
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.is_in_group("explosions") and not hit_by_rocket and explode_timer.time_left > 0:
+		print("debug")
 		explode_timer.stop()
 		var explosion_instance = explosion.instantiate()
 		explosion_instance.global_position = global_position
 		explosion_instance.explosion_strength *= 1.5
-		explosion_instance.scale *= 1.5
+		print(explosion_instance.explosion_strength)
+		explosion_instance.scale *= 1.2
 		get_parent().add_child(explosion_instance)
 		queue_free()
+
+
+func _on_explode_timer_timeout() -> void:
+	var explosion_instance = explosion.instantiate()
+	explosion_instance.global_position = global_position
+	get_parent().add_child(explosion_instance)
+	queue_free()
