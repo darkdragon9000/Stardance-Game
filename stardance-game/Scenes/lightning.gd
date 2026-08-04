401768932c52f24f8d8267cbc3fa8176195c7f00
Xblader1359 <Xblader1359.Minecraft@gmail.com>
2026-08-04 10:02:18 -0500

    fix lightning a bit

--- stardance-game/Scenes/lightning.gd
+++ stardance-game/Scenes/lightning.gd
@@ -3,6 +3,7 @@ extends AnimatedSprite2D
 
 @onready var lightning_timer: Timer = $LightningTimer
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	frame = 7
@@ -15,9 +16,14 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_lightning_timer_timeout() -> void:
-	position = Vector2(randf_range(-250, 250), randf_range(50, 150))
-	rotation = randf_range(-30, 30)
+	position = Vector2(randf_range(-250, 250), randf_range(0, 150))
 	scale = Vector2(randf_range(0.5, 1.5), randf_range(0.5, 1.5))
 	animation = str(randi_range(1, 3))
+	if animation != "1":
+		rotation = deg_to_rad(randf_range(-15, 15))
+	else:
+		rotation = deg_to_rad(randf_range(-5, 5))
+	print(rad_to_deg(rotation))
+	position.y += (1 - scale.y) * 20
 	play()
 	lightning_timer.wait_time = randf_range(0.05, 3.0)
